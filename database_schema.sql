56a25ab43e1eefc9e645d9d6cce47cb77db56172
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-06-18 16:41:14 -0500

    Commit for peer review purposes

--- database_schema.sql
+++ database_schema.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE weekend_challenge_04;
 
 -- Command to create new table in Postico
-CREATE TABLE todolist
+CREATE TABLE list
 ( id SERIAL PRIMARY KEY NOT NULL,
   task VARCHAR(30) UNIQUE,
   category VARCHAR(30),
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,13 +3,14 @@ console.log('javascript is sourced!');
 $(document).ready(function() {
   console.log('jQuery is sourced!');
   // display current To Do list on application load
-  $.ajax({
-    type: 'GET',
-    url: '/getList',
-    success: function (data) {
-    console.log('Got the list:' + displayCurrentList(data));
-    } // end success
-  }); //end ajax request
+  listToDom();
+  // $.ajax({
+  //   type: 'GET',
+  //   url: '/getList',
+  //   success: function (data) {
+  //   console.log('Got the list:' + displayCurrentList(data));
+  //   } // end success
+  // }); //end ajax request
 
   $('#newTaskBtn').on('click', function () {
     // event.preventDefault();
@@ -29,18 +30,22 @@ $(document).ready(function() {
     $.ajax({
       type: 'POST',
       url: '/postNewTask',
-      data: objectToSend
+      data: objectToSend,
+      success: function () {
+        listToDom();
+      }
     }); // end ajax request
     $('#newTask').val(''); // empty input field after a task is added by user
     // $('.outputDiv').empty(); // refresh div each time so no duplicate tasks
 
-    $.ajax({
-      type: 'GET',
-      url: '/getList',
-      success: function (data) {
-        console.log('Got the list:' + displayCurrentList(data));
-      } // end success function
-    }); //end ajax request
+    // $.ajax({
+    //   type: 'GET',
+    //   url: '/getList',
+    //   success: function (data) {
+    //     displayCurrentList(data);
+    //     console.log('Got the list:' + displayCurrentList(data));
+    //   } // end success function
+    // }); //end ajax request
     // $('.outputDiv').empty(); // refresh div each time so no duplicate tasks
   }); // end add task click function
 
@@ -50,6 +55,18 @@ $(document).ready(function() {
     $(this).closest('.row').find('.outputDiv').css('text-decoration', 'line-through').css('opacity', '0.50');
   });
 
+
+  function listToDom () {
+    $.ajax({
+      type: 'GET',
+      url: '/getList',
+      success: function (data) {
+        displayCurrentList(data);
+        console.log('Got the list:' + displayCurrentList(data));
+      } // end success function
+    }); //end ajax request
+  } // end listToDom function
+
   function displayCurrentList (list) {
     //  console.log( 'in displayCurrentList:' + list );
      for (i=0; i<list.length; i++) {
@@ -57,6 +74,5 @@ $(document).ready(function() {
        var todoList = $('#task').find('.outputDiv').append('<p>'+list[i].task+'</p>') + $('#category').find('.outputDiv').append('<p>'+list[i].category+'</p>') + $('#action').find('.outputDiv').append('<p><button type="button" id="complete">Complete</button><button type="button" id="delete">Delete</button></p>');
       //  $('#outputDiv').append(todoList);
      } // end for loop
-   } // end displayZoo function
-
+   } // end displayCurrentList function
 }); // end doc ready function
